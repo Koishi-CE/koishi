e5f741a4d2a7f383bac8d5918a00ae2f8233b596
oppenheymu <oppenheymu@gmail.com>
2026-09-01 22:15:19 +0800

    license: 为根配置与 issue 模板添加 SPDX 版权头（本仓原创）

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026-present Koishi-CE contributors.
+
 import tsParser from "@typescript-eslint/parser";
 import type { Linter } from "eslint";
 import pluginVue from "eslint-plugin-vue";
